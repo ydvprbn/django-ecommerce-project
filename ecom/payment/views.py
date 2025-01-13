b98ef511c4a7ea2e852eed8fd0c360014e954add
Prabin Yadav <ydprabin11@gmail.com>
2025-01-13 14:09:20 +0545

    initial commit

---
+++ ecom/payment/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress
+from django.contrib import messages
+
+
+def billing_info(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Check to see if user is logged in
+        if request.user.is_authenticated:
+            # Get The Billing Form
+            billing_form = PaymentForm()
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+        else:
+            # Not logged in
+            billing_form = PaymentForm()
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+
+        shipping_form = request.POST
+        return render(
+            request,
+            "payment/billing_info.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        messages.success(request, "Access Denied!")
+        return redirect("home")
+
+
+def checkout(request):
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+        # Checkout as logged in user
+        # Shipping User
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        # Shipping Form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(
+            request,
+            "payment/checkout.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        # Checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        return render(
+            request,
+            "payment/checkout.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+
+
+def payment_success(request):
+    return render(request, "payment/payment_success.html", {})
