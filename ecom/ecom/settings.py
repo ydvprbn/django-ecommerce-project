f32355a63c7661d3a25a98d097f7c5854b5fdfd7
ydvprbn <prabin.ydv098@gmail.com>
2025-01-23 21:07:29 +0545

    Added build files

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = ["static/"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # White noise static stuff
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
