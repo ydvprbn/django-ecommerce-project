2899e95fdbf71d469b588342e4dc7d34af30eac2
ydvprbn <prabin.ydv098@gmail.com>
2025-01-24 00:02:23 +0545

    Updated build files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
+mkdir -p /ecom
+pip install -r requirements.txt 
+
 mkdir -p /ecom/ecom
 cd /ecom/ecom
-cp requirements.txt requirements.txt
-pip install -r requirements.txt 
+
 python3.9 manage.py collectstatic
