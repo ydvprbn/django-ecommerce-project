e2e9e33b77301d113eb339c8dacc00924f328bbc
ydvprbn <prabin.ydv098@gmail.com>
2025-01-24 00:55:07 +0545

    updated vercel file

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # White noise static stuff
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
