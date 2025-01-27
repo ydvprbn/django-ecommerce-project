492f71961f2dc5a695b26a278ccae388ccd88c66
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 12:17:37 +0545

    updated settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -87,23 +87,25 @@ WSGI_APPLICATION = "ecom.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "railway",
-#         "USER": "postgres",
-#         "PASSWORD": os.environ["password"],
-#         "HOST": "monorail.proxy.rlwy.net",
-#         "PORT": "33961",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+password = os.environ["password"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": password,
+        "HOST": "monorail.proxy.rlwy.net",
+        "PORT": "33961",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
