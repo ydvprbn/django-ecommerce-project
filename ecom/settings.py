cb3f608ea7660145c1e6edb21854022ebd61a209
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 17:23:03 +0545

    updated settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #     "django-ecommerce-project-production-908d.up.railway.app",
