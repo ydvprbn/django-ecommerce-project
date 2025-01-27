7562a0c0132eeb0b31f440ac29402e7057efe0b3
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 12:21:05 +0545

    updated settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,11 +28,11 @@ SECRET_KEY = os.environ.get("secret_key")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "django-ecommerce-project-production-369e.up.railway.app",
-    "https://django-ecommerce-project-production-369e.up.railway.app",
+    "django-ecommerce-project-production-5dde.up.railway.app",
+    "https://django-ecommerce-project-production-5dde.up.railway.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://django-ecommerce-project-production-369e.up.railway.app",
+    "https://django-ecommerce-project-production-5dde.up.railway.app",
 ]
 
 # Application definition
