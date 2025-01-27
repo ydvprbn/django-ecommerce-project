d37d9e5c69843363145970862e35212ca191500c
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 22:00:52 +0545

    updated settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -145,7 +145,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "https://django-ecommercefiles.s3.amazonaws.com/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
@@ -166,10 +165,13 @@ PAYPAL_RECEIVER_EMAIL = "business@testcode.com"  # Business Sandbox account
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.environ.get("ap-southeast-2")
-AWS_S3_SIGNATURE_VERSION = os.environ.get("s3v4")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = os.environ.get("AWS_S3_SIGNATURE_VERSION")
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
