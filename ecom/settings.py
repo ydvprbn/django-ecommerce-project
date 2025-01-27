9e2c4f87dccdaab234e0599df288cfd550c00252
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 22:31:23 +0545

    updated settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -25,15 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-# ALLOWED_HOSTS = [
-#     "django-ecommerce-project-production-908d.up.railway.app",
-#     "https://django-ecommerce-project-production-908d.up.railway.app",
-# ]
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://django-ecommerce-project-production-908d.up.railway.app",
-# ]
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
