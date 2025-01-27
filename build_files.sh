ecc962e2f0a8c3ae8e1ec996925817cd450fffd7
ydvprbn <prabin.ydv098@gmail.com>
2025-01-27 15:53:25 +0545

    updated build files & settings

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,8 @@
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
+# Run database migrations
+python3.9 ecom/manage.py migrate --noinput
+
 # Run collectstatic (ensure the path to manage.py is correct)
 python3.9 ecom/manage.py collectstatic --noinput
